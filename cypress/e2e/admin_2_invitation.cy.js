94c3bb2dcddb66c1689261e85a6172f5ae0fe06e
Johnattan Devia <jd@ingenio.com.co>
2022-11-11 21:34:10 -0500

    Cypress update invitations tests

---
+++ cypress/e2e/admin_2_invitation.cy.js
@@ -0,0 +1,46 @@
+import {faker} from "@faker-js/faker";
+
+describe('Admin create elements in configuration', () => {
+
+  it('Como usuario administrador envio una invitación a un nuevo usuario', () => {
+
+    const email = faker.lorem.words(1) + (Date.now()) + '@' + faker.internet.domainName()
+
+    cy.intercept('/ghost/api/admin/invites/*').as('saveSettings')
+
+
+    cy.goAdminAndLogin()
+    cy.goIntoSettings('staff')
+
+    let previewLength = 0
+    cy.wait(2000).then(() => {
+        if (Cypress.$('.apps-configured a[href="#revoke"]').length > 0) {
+            cy.get('.apps-configured a[href="#revoke"]').then(($aElement) => {
+                previewLength = $aElement.length
+            })
+        }
+    })
+
+    cy.get(".view-actions button.gh-btn-primary").click()
+    cy.wait(1000)
+    cy.get('#new-user-email').clear().type(email)
+    cy.get('.gh-roles-container .gh-radio .gh-radio-label').contains(faker.helpers.arrayElement(['Author','Editor','Administrator'])).click()
+    cy.get('.modal-footer button').click()
+    cy.wait('@saveSettings')
+    cy.reload()
+
+    cy.get('.apps-configured a[href="#revoke"]').should(($aElement) => {
+      expect($aElement).to.have.length(previewLength + 1)
+    })
+
+    cy.get('.apps-configured a[href="#revoke"]').first().click()
+    cy.wait('@saveSettings')
+    cy.reload()
+
+    cy.get('.apps-configured a[href="#revoke"]').should(($aElement) => {
+      expect($aElement).to.have.length(previewLength)
+    })
+
+  })
+
+})
